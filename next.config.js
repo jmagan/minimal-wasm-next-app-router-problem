475d02880e3f3dcd4286c37df8b6ad97239f86df
Torsten Schmitz <git@torsten.dev>
2024-01-18 22:57:33 +0100

    fixed it

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+    }
+    return config
+  },
 }
+
+module.exports = nextConfig;
